53058eb6cbc8f5706ce11bce64e1e77b5f645ebf
krautz01 <reichskanzler46@gmail.com>
2021-10-04 15:01:33 +0600

    компоненты

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wellcome to Germany <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 }
