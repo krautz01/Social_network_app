b3c979060e912157ffbd1f0479110acd2acbab3a
krautz01 <reichskanzler46@gmail.com>
2021-10-05 14:47:30 +0600

    layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-//import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
-import Technologies from './Technologies.js';
+import Technologies from './Technologies';
+import Footer from './Footer.js';
 
 function App() {
   return (
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+//import React from 'react';
+
+const Footer = () => {
+    return (<div>
+        it-reich.com footer
+    </div>);
+}
+
+export default Footer;
