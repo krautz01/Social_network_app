cc72ba04e626645308592c68dc6c5ec8e1213522
krautz01 <reichskanzler46@gmail.com>
2021-10-07 20:31:00 +0600

    crash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+//import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
@@ -8,10 +9,13 @@ function App() {
   return (
     <div className='app-wrapper'>
       <Header />
-      <Navbar />
-      <Profile />
+      <div className='app-content'>
+        <Navbar />
+        <Profile />
+        {/*<Dialogs />*/}
+      </div>
     </div>
   );
 }
- 
+
 export default App;
