2a3856abd3f00b36e95b994d31403e1d4fcdf3a3
krautz01 <reichskanzler46@gmail.com>
2021-10-07 23:49:07 +0600

    routing end

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
@@ -7,14 +8,16 @@ import Profile from './components/Profile/Profile';
 
 function App() {
   return (
-    <div className = 'app-wrapper'>
-      <Header />
-      <Navbar />
-      {/*<Profile />*/}
-      <div className = 'app-content-wrapper'>
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-content-wrapper'>
+          <Route path = '/dialogs' component={Dialogs} />
+          <Route path = '/profile' component={Profile} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
     return (
       <nav className = {s.nav}>
         <div className = {s.item}>
-          <a>Profile</a>
+          <a href = "/profile">Profile</a>
         </div>
         <div className = {'${s.item} ${s.active'}>
-          <a href = "/profile">Massages</a>
+          <a href = "/dialogs">Massages</a>
         </div>
         <div className = {s.item}>
-          <a href = "/dialogs">News</a>
+          <a href = "/news">News</a>
         </div>
         <div className = {s.item}>
-          <a href = "/profile">Music</a>
+          <a href = "/music">Music</a>
         </div>
         <div className = {s.item}>
-          <a>Settings</a>
+          <a href = "/settings">Settings</a>
         </div>
       </nav>
     )
