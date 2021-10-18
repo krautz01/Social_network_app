1819035f9fbfa70697c5718481b88cc9e516dfe5
krautz01 <reichskanzler46@gmail.com>
2021-10-18 14:43:39 +0600

    ff

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,6 +12,7 @@ const MyPosts = (props) => {
         debugger;
         let text = newPostElement.current.value;
         props.addPost(text);
+        newPostElement.current.value = '';
     }
 
     return (
