27e71896ff984fd76699e12af26a86ac661e47e3
krautz01 <reichskanzler46@gmail.com>
2021-11-24 19:27:39 +0600

    reducer

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,7 @@
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
-import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/state';
+import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogs_reducer';
 
 const Dialogs = (props) => {
     let state = props.store.getState().dialogsPage;
