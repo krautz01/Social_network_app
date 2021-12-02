776ea2f62de3a58af7126e435a16a369401cf470
krautz01 <reichskanzler46@gmail.com>
2021-12-02 16:26:31 +0600

    copys

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,8 @@ import DialogItem from './DialogItem/DialogItem';
 
 const Dialogs = (props) => {
     let state = props.dialogsPage;
-    let dialogsElements = state.dialogs.map ( (d) => <DialogItem name={d.name} id={d.id}/> );
-    let messagesElements = state.messages.map ( (m) => <Message message={m.message}/> );
+    let dialogsElements = state.dialogs.map ( (d) => <DialogItem name = {d.name} key = {d.id} id = {d.id} /> );
+    let messagesElements = state.messages.map ( (m) => <Message message = {m.message} key = {m.id} /> );
     let newMessageBody = state.newMessageBody;
 
     let onSendMessageClick = () => {
@@ -17,15 +17,15 @@ const Dialogs = (props) => {
     }
     
     return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
+        <div className = {s.dialogs}>
+            <div className = {s.dialogsItems}>
                 {dialogsElements}
             </div>
             <div className = {s.messages}>
                 <div>{messagesElements}</div>
                 <div>
-                    <div><textarea value={newMessageBody} onChange={onNewMessageChange} placeholder='Enter yoour message'></textarea></div>
-                    <div><button onClick={ onSendMessageClick }>Send</button></div>
+                    <div><textarea value = {newMessageBody} onChange = {onNewMessageChange} placeholder = 'Enter yoour message'></textarea></div>
+                    <div><button onClick = { onSendMessageClick }>Send</button></div>
                 </div>
             </div>
         </div>
--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,28 +1,6 @@
-import React from 'react';
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogs_reducer';
 import Dialogs from './Dialogs';
 import { connect } from 'react-redux';
-
-/* const DialogsContainer = () => {
-    return <StoreContext.Consumer>
-        {(store) => {
-            //let state = store.getState().dialogsPage;
-
-            let onSendMessageClick = () => {
-                store.dispatch(sendMessageCreator());
-            }
-            let onNewMessageChange = (body) => {
-                store.dispatch(updateNewMessageBodyCreator(body));
-            }
-
-            return <Dialogs updateNewMessageBody={onNewMessageChange}
-                sendMessage={onSendMessageClick}
-                dialogsPage={state}
-            />
-
-        }} 
-        </StoreContext.Consumer>
-} */
 
 let mapStateToProps = (state) => {
     return {
@@ -31,12 +9,13 @@ let mapStateToProps = (state) => {
 }
 
 let mapDispatchToProps = (dispatch) => {
+    
     return {
         updateNewMessageBody: (body) => {
-            dispatch(updateNewMessageBodyCreator(body));////////
+            dispatch(updateNewMessageBodyCreator(body));
         },
         sendMessage: () => {
-            dispatch(sendMessageCreator());/////////////
+            dispatch(sendMessageCreator());
         }, 
     }
 }
