819383abee3d61c64e2c4bfd9efcbad62f065035
krautz01 <reichskanzler46@gmail.com>
2021-12-11 15:37:24 +0600

    55

--- src/redux/users_reducer.js
+++ src/redux/users_reducer.js
@@ -1,42 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-    users: [/* 
-            {
-                id: 1, 
-                photoURL: 'https://www.youloveit.ru/uploads/posts/2020-04/1586360515_youloveit_ru_dipper_gravity_falls_na_avu02.jpg',
-                followed: true, 
-                fullName: "Hans", 
-                status: "Ich bin Ingeneur", 
-                location: { city: 'Konigsberg', country: 'Germany' }
-            },
-            {
-                id: 2, 
-                photoURL: 'https://www.youloveit.ru/uploads/posts/2020-04/1586363751_youloveit_ru_meibl_gravity_falls_na_avu16.jpg',
-                followed: true, 
-                fullName: "Marta", 
-                status: "Ich bin Ingeneur", 
-                location: { city: 'Milan', country: 'Italy' }
-            },
-            {
-                id: 3, 
-                photoURL: 'https://www.youloveit.ru/uploads/posts/2020-04/1586361975_youloveit_ru_wendy_gravity_falls_na_avu09.jpg',
-                followed: false, 
-                fullName: "Luisa", 
-                status: "Ich bin Ingeneur", 
-                location: { city: 'Berlin', country: 'Germany' }
-            },
-            {
-                id: 4, 
-                photoURL: 'https://slovnet.ru/wp-content/uploads/2018/09/1-61.jpg',
-                followed: false, 
-                fullName: "Erika", 
-                status: "Ich bin Ingeneur", 
-                location: { city: 'Berlin', country: 'Germany' }
-            }, */
-        ]
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -62,8 +34,15 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS: {
-            return { ...state, users: [...state.users, ...action.users] }
+            return { ...state, users: action.users }
         }
+        case SET_CURRENT_PAGE: {
+            return { ...state, currentPage: action.currentPage }
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return { ...state, totalUsersCount: action.count }
+        }
+        
         default:
             return state;
     }
@@ -72,5 +51,8 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userID) => ({ type: FOLLOW, userID })
 export const unfollowAC = (userID) => ({ type: UNFOLLOW, userID })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setTotalUsersCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
+
 
 export default usersReducer;
